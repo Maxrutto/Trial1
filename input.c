00de5d086997d46d20c5ff60e47777c0f1d70afd
Maxrutto <ruttomaxmillan@gmail.com>
2023-08-24 13:46:28 -0700

    File opening

---
+++ input.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * f_read - Checks if a file exists and read or opened
+ * @argc: Argument count
+ * @argv: Arguments
+ *
+ * Return: File pointer
+ */
+
+FILE *f_read(int argc, char **argv)
+{
+	FILE *fp;
+
+	if (argc > 2 || argc == 1)
+	{
+		dprintf("2, USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(argv[1], "r");
+
+	if (fd == NULL)
+	{
+		dprintf(2, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	return (fp);
+}
