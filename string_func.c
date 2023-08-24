abfce6495cef9dce5a9b28b3a1ad7da783d3d465
Maxrutto <ruttomaxmillan@gmail.com>
2023-08-24 11:25:44 -0700

    More changes

--- string_func.c
+++ string_func.c
@@ -22,3 +22,29 @@ int is_int(const char *str)
 
 	return (1);
 }
+
+/**
+ * str_cmp - Function that compares two strings
+ * @str1: First string
+ * @str2: Second string
+ *
+ * Return: 0 if str1 = str2
+ */
+
+int str_cmp(char *str1, char *str2)
+{
+	while (*str1 != '\0' && *str2 != '\0')
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+	}
+
+	if (*str == '\0' && *str2 != '\0')
+		return -(*str2);
+	else if (*str1 != '\0' && *str2 == '\0')
+		return (*str1);
+
+	return (0);
+}
